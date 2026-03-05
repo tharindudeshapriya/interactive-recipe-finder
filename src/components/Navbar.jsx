73e13822a54d426cfca570922bb63c8a80600334
tharindudeshapriya <tmdeshapriya@gmail.com>
2026-03-05 13:22:28 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,15 +15,19 @@ const Navbar = () => {
         setIsOpen(false);
     };
 
-    // Prevent scrolling when menu is open
+    // Prevent scrolling when menu is open without causing layout shift
     useEffect(() => {
         if (isOpen) {
+            const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
             document.body.style.overflow = 'hidden';
+            document.body.style.paddingRight = `${scrollbarWidth}px`;
         } else {
-            document.body.style.overflow = 'unset';
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '';
         }
         return () => {
-            document.body.style.overflow = 'unset';
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '';
         };
     }, [isOpen]);
 
