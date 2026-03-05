73c98940ff06f4463316f592d4dbfe0b45546659
tharindudeshapriya <tmdeshapriya@gmail.com>
2026-03-06 00:01:08 +0530

    minor changes
    move to top of the page forward, move to same position when backward

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import Categories from './pages/Categories'
 import CategoryDetails from './pages/CategoryDetails'
 import ScrollToTopButton from './components/layout/ScrollToTopButton'
 import useLookupData from './hooks/useLookupData'
+import ScrollManager from './components/layout/ScrollManager'
 
 function App() {
   useLookupData();
 
   return (
     <Router>
+      <ScrollManager />
       <div className="min-h-screen flex flex-col bg-bg-base text-text-base font-sans transition-colors duration-300">
         <Navbar />
 
